139155d0fb859a5421d6e2db4b9b1ad8e79de66a
rifatabrajowad <rajdiprifat2004@gmail.com>
2023-06-04 21:28:16 +0600

    site setup

---
+++ site/resources/views/Home.blade.php
@@ -0,0 +1,7 @@
+@extends('Layout.app')
+@section('content')
+@include('Component.Banner')
+@include('Component.Product')
+
+
+@endsection
---
+++ site/resources/views/Layout/menu.blade.php
@@ -0,0 +1,37 @@
+<header class="header_section">
+    <div class="header_bottom">
+        <div class="container-fluid">
+            <nav class="navbar navbar-expand-lg custom_nav-container">
+                <a class="navbar-brand" href="index.html">
+                    <span> Minics </span>
+                </a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class=""> </span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="about.html"> About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="product.html">Products</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="why.html">Why Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="testimonial.html">Testimonial</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+</header>
